b0ec6e3ce7e1099418a1cc7f90bf4239a35c174c
Samanta Cicilia <samycici@gmail.com>
2020-11-15 11:32:51 -0300

    add new structure

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -1,5 +1,5 @@
-describe('Teste Automatizado', function (){
-  it('Mentoria', function (){
+describe('Cadastro', function (){
+  it('Cadastrar novo registro com sucesso', function (){
     cy.visit('http://demo.automationtesting.in/Register.html')
 
     cy.get('input[placeholder="First Name"]')
@@ -15,7 +15,7 @@ describe('Teste Automatizado', function (){
       .type('saphira@gmail.com')
     
     cy.get('input[type="tel"]') //phone
-      .type('+55(66)99999-9999')
+      .type('5417543010')
    
     cy.get('input[value="FeMale"]').click() //genêro
 
@@ -51,12 +51,14 @@ describe('Teste Automatizado', function (){
       .select("14")
 
     cy.get('input[id="firstpassword"]') //senha
-      .type("12345678")
+      .type("Ertar1345%")
    
     cy.get('input[id="secondpassword"]') //confirmar senha
-      .type("12345678")   
+      .type("Ertar1345%")   
 
     cy.get('button[id="submitbtn"]').click()
+
+    cy.url().should('include', '/WebTable.html') //verifica se a nova URL tem /WebTable.html
     
     })
 })              
